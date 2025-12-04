cc7ecff0f717a1046b2897abc0838456a0ad30cb
Valentina Danilova <dvalentina.frontend@gmail.com>
2025-12-04 23:32:30 +0400

    fix: fix collapsible styles

--- src/components/faq-section/collapsible-question.tsx
+++ src/components/faq-section/collapsible-question.tsx
@@ -14,15 +14,15 @@ function CollapsibleQuestion({
   answer: string;
 }) {
   return (
-    <Collapsible className="group w-full bg-bg-primary border border-border-primary rounded-lg px-8 py-6 data-[state=open]:border-border-tertiary overflow-hidden">
-      <CollapsibleTrigger className="w-full cursor-pointer text-h5 text-content-dark-primary text-left flex justify-between items-center">
+    <Collapsible className="group w-full bg-bg-primary hover:bg-bg-secondary border border-border-primary rounded-lg  data-[state=open]:border-border-tertiary overflow-hidden">
+      <CollapsibleTrigger className="w-full h-full px-8 py-6 cursor-pointer text-h5 text-content-dark-primary text-left flex justify-between items-center">
         {question}
         <div>
           <CaretDown className="block group-data-[state=open]:hidden" />
           <CaretUp className="hidden group-data-[state=open]:block" />
         </div>
       </CollapsibleTrigger>
-      <CollapsibleContent className="text-body-xl text-content-dark-secondary font-normal data-[state=open]:animate-collapsible-slide-down data-[state=closed]:animate-collapsible-slide-up data-[state=open]:mt-4">
+      <CollapsibleContent className="px-8 data-[state=open]:pb-6 data-[state=open]:-mt-2 text-body-xl text-content-dark-secondary font-normal data-[state=open]:animate-collapsible-slide-down data-[state=closed]:animate-collapsible-slide-up">
         {answer}
       </CollapsibleContent>
     </Collapsible>
