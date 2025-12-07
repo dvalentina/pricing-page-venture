4692989c7ec2a7965dc5c769500bbe606b569c6a
Valentina Danilova <dvalentina.frontend@gmail.com>
2025-12-08 00:09:11 +0400

    feat: add pricing cards appear animation

--- src/components/pricing-section/pricing-section.tsx
+++ src/components/pricing-section/pricing-section.tsx
@@ -5,6 +5,7 @@ import { useBillingFrequencyContext } from "../toggle-billing-frequency/billing-
 import ToggleBillingFrequency from "../toggle-billing-frequency/toggle-billing-frequency";
 import { cards } from "./constants";
 import MarqueeLogos from "./marquee-logos";
+import { motion } from "motion/react";
 
 function PricingSection() {
   const { billingFrequency } = useBillingFrequencyContext();
@@ -12,17 +13,41 @@ function PricingSection() {
   return (
     <section className="w-full flex justify-center items-center">
       <div className="w-full max-w-(--content-max-width) flex flex-col gap-16 justify-center items-center px-20">
-        <ToggleBillingFrequency />
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{
+            type: "spring",
+            duration: 0.6,
+          }}
+        >
+          <ToggleBillingFrequency />
+        </motion.div>
         <div className="w-full flex gap-8">
-          {cards.map((card) => {
+          {cards.map((card, index) => {
             const costs =
               billingFrequency === "yearly" ? card.annually : card.monthly;
             return (
-              <PricingCard key={card.title} accent={card.title === "Pro"}>
-                <PricingCard.Title title={card.title} {...costs} />
-                <PricingCard.Content {...card.content} />
-                <PricingCard.CTA>{card.callToAction}</PricingCard.CTA>
-              </PricingCard>
+              <motion.div
+                key={card.title}
+                initial={{ y: 30, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{
+                  type: "spring",
+                  bounce: 0.4,
+                  duration: 0.6,
+                  delay: index * 0.1,
+                }}
+                className="flex w-full"
+              >
+                <PricingCard accent={card.title === "Pro"}>
+                  <PricingCard.Title title={card.title} {...costs} />
+                  <PricingCard.Content {...card.content} />
+                  <PricingCard.CTA>{card.callToAction}</PricingCard.CTA>
+                </PricingCard>
+              </motion.div>
             );
           })}
         </div>
